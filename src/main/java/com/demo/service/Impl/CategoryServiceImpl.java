3070cc932ff8682642904421c9581016fbc2c230
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2024-02-10 00:30:19 +0900

    カテゴリINSERT部分を大雑把に作成

--- src/main/java/com/demo/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/demo/service/Impl/CategoryServiceImpl.java
@@ -9,15 +9,23 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
-@Transactional
 @Service
+@Transactional
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryMapper categoryMapper;
 
+    /** 新規作成 */
+    @Override
+    public void insert(Category category) {
+        categoryMapper.insert(category);
+    }
+
+    /** 全件取得 */
     @Override
     public List<Category> selectAll() {
         return categoryMapper.selectAll();
     }
+
 }
